f717acf1d018bca6ad3f0df917afeac0ef776db0
smohiudd <saadiq@developmentseed.org>
2023-06-09 15:09:01 -0600

    permission boundary

--- terraform/modules/aws_ecs_service/main.tf
+++ terraform/modules/aws_ecs_service/main.tf
@@ -25,6 +25,7 @@ resource "aws_iam_role" "ecs_execution_role" {
   name               = "${var.service_name}-${var.environment}_ecs_task_execution_role"
   assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
   tags               = var.tags
+  permissions_boundary = "arn:aws:iam::444055461661:policy/mcp-tenantOperator"
 }
 
 data "aws_iam_policy_document" "ecs_execution_attachment" {
