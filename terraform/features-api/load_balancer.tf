ceeabe8e4b82ebcd57328a65a33fc2dc84c0f67d
smohiudd <saadiq@developmentseed.org>
2023-06-12 13:10:03 -0600

    lb ingress 443 rule

--- terraform/features-api/load_balancer.tf
+++ terraform/features-api/load_balancer.tf
@@ -23,6 +23,13 @@ resource "aws_security_group" "web_inbound_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
