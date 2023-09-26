f9873075f6ce7f284a0c61790ce161704a732fb3
ranchodeluxe <greg.corradini@gmail.com>
2023-09-26 13:48:11 -0700

    changes

--- veda-wfs3-app/startup.sh
+++ veda-wfs3-app/startup.sh
@@ -1,9 +1,13 @@
 ################################################################
 # FAST API
 #################################################################
+echo "######## DB SETUP ########" | tee /dev/stderr
 echo $DB_CONFIG | jq -r "to_entries|map(\"export \(.key)=\(.value|tostring)\")|.[]" > .env
 chmod 777 .env && source .env
 psql -d "postgresql://${username}:${password}@${host}/${dbname}" -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psqloutput=$(psql -d "postgresql://${username}:${password}@${host}/${dbname}" -c "SELECT extname FROM pg_extension WHERE extname = 'postgis';")
+echo $psqloutput | tee /dev/stderr
+echo "######## END DB SETUP ########" | tee /dev/stderr
 
 if [ "$ENVIRONMENT" == "prod" ]; then
   /opt/bitnami/python/bin/uvicorn fast_api_main:app --proxy-headers --forwarded-allow-ips="*"  --host 0.0.0.0 --port 8080
